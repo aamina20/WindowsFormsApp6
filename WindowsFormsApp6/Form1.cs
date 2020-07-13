d4a583e2fe1b80fe7d501517920c02a216f315f2
aamina20 <67161701+aamina20@users.noreply.github.com>
2020-07-13 16:29:35 +0400

    Добавьте файлы проекта.

---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        bool vis = true;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "Admin" & textBox2.Text == "1234567890")
+            {
+                this.Hide();
+                Form2 form2 = new Form2();
+                form2.Show();
+            }
+            else
+            {
+                MessageBox.Show("Не правильно введен логин или пароль!");
+            }
+        }
+       
+
+
+
+        
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (vis)
+            {
+                textBox2.UseSystemPasswordChar = false;
+                vis = false;
+            }
+            else
+            {
+                textBox2.UseSystemPasswordChar = true;
+                vis = true;
+            }
+        }
+    }
+}
